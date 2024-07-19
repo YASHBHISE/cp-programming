e3d01b76ceb1e08a970ec75691c1c9463bbc53df
Yash Nilesh Bhise <92324872+YASHBHISE@users.noreply.github.com>
2024-07-19 19:24:27 +0530

    Add files via upload

---
+++ Stack/tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+stack<char>st;
+        for(auto it:s){
+            if(it=='(' || it=='[' || it=='{'){
+                st.push(it);
+            }else{
+                if(st.size() == 0) return false;
+                char ch = st.top();
+                st.pop();
+
+                if((it==')' && ch == '(') ||(it==']' && ch == '[') || (it=='}' && ch == '{')){
+                    continue;
+                }
+                else return false;
+            }
+            st.empty();
+        }
